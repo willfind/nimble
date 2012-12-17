5c26e4ae0d9aa40541844d09c72b868d412de142
Spencer <willfind@gmail.com>
2012-12-17 10:28:18 -0500

    cleaned up a few mistakes and issues with the CrossValidate code

--- combinations/CrossValidate.py
+++ combinations/CrossValidate.py
@@ -1,5 +1,5 @@
 import Combinations
-import DataMatrix
+#import DenseMatrix
 
 def runAlgorithm(train, test, algorithm, parameters):
 	pass #this would already be implemented in other UML code
@@ -18,13 +18,13 @@ def crossValidate(X, Y, functionsToApply, numFolds=10):
 		YIterator = Y.foldIterator(numFolds=numFolds)
 		while True: #need to add a test here for when iterator .next() is done
 			try:
-				curtrainX, curtestX = XIterator.next()
-				curtrainY, curtestY = YIterator.next()
+				curTrainX, curTestX = XIterator.next()
+				curTrainY, curTestY = YIterator.next()
 			except StopIteration:	#once we've gone through all the folds, this exception gets thrown and we're done!
 					break
 			dataHash = {}
-			dataHash["trainX"] = curtrainX; dataHash["testX"] = curtestX	#assumes that the function text in functionsToApply uses these variables
-			dataHash["trainY"] = curtrainY; dataHash["testY"] = curtestY
+			dataHash["trainX"] = curTrainX; dataHash["testX"] = curTestX	#assumes that the function text in functionsToApply uses these variables
+			dataHash["trainY"] = curTrainY; dataHash["testY"] = curTestY
 			curResults.append(Combinations.executeCode(function, dataHash))
 		aggregatedResults[function] = sum(curResults)/float(len(curResults))
 	return aggregatedResults
@@ -53,12 +53,12 @@ def normalize(train, test, algorithm, parameters=None):
 	train.copyOf(runAlgorithm(train, test, algorithm=algorithm, parameters=parameters))	#copyOf() would set train so that it is the same as what's passed to it
 	test.copyOf(runAlgorithm(train, train, algorithm=algorithm, parameters=parameters))	#copyOf() would set test so that it is the same as what's passed to it
 
-def loadTrainingAndTesting(fileName, labelID, fractionForTesting):
+def loadTrainingAndTesting(fileName, labelID, fractionForTestSet):
 	"""this is a helpful function that makes it easy to do the common task of loading a dataset and splitting it into training and testing sets.
 	It returns training X, training Y, testing X and testing Y"""
-	trainX = DataMatrix.DataMatrix("myFile.txt")	#load all our data (both X & Y) I'm not sure actually how we do this in the current code
+	trainX = DenseMatrix.DenseMatrix("myFile.txt")	#load all our data (both X & Y) I'm not sure actually how we do this in the current code
 													#we'll have to deal with different data types (dense, sparse, etc.) but I'm ignoring that here
-	testX = trainX.extractPoints(number=int(round(fractionForTesting*len(trainX))), randomize=True)	#pull out a testing set
+	testX = trainX.extractPoints(number=int(round(fractionForTestSet*len(trainX))), randomize=True)	#pull out a testing set
 	trainY = trainX.extractFeatures(0)	#construct the column vector of training labels
 	testY = testX.extractFeatures(0)	#construct the column vector of testing labels
 	return trainX, trainY, testX, testY
@@ -72,7 +72,7 @@ if __name__ == "__main__":
 	###############################################
 	#mean normalize your training and testing data#
 
-	trainX, trainY, testX, testY = loadTrainingAndTesting("myFile.txt", "predictionLabels", fractionForTesting=.15) #load and split up the data
+	trainX, trainY, testX, testY = loadTrainingAndTesting("myFile.txt", "predictionLabels", fractionForTestSet=.15) #load and split up the data
 	normalize(trainX, testX, algorithm="mean") #perform mean normalization
 
 	##################################################
@@ -92,7 +92,7 @@ if __name__ == "__main__":
 	runs = Combinations.functionCombinations('runAlgorithmWithPeformance(trainX, trainY, testX, testY, algorithm="mlpy.svm", parameters={"C":<0.1|1|10|100>, "iterations":<100|1000>})')
 
 	#this will return the text of whichever function performed better, as well as that best performance value
-	bestFunction, performance = crossValidateReturnBest(trainX, trainY, [run1, run2], numFolds=10)
+	bestFunction, performance = crossValidateReturnBest(trainX, trainY, runs, numFolds=10)
 
 
 
