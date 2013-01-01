40be2d43a7e297179b3540ee32db73b3ef637721
Trevor Burns <trevorpburns@gmail.com>
2013-01-01 16:24:55 -0500

    Issue #5: actually modified CooSparseData to conform to new extractColumns api

--- processing/coo_sparse_data.py
+++ processing/coo_sparse_data.py
@@ -22,8 +22,58 @@ class CooSparseData(SparseData):
 		super(CooSparseData, self).__init__(self.data,featureNames)
 
 
+	def _extractColumns_implementation(self, toExtract, start, end, number, randomize):
+		"""
+		Function to extract columns according to the parameters, and return an object containing
+		the removed columns with their featureName names from this object. The actual work is done by
+		further helper functions, this determines which helper to call, and modifies the input
+		to accomodate the number and randomize parameters, where number indicates how many of the
+		possibilities should be extracted, and randomize indicates whether the choice of who to
+		extract should be by order or uniform random.
 
-	def _extractColumns_implementation(self,toExtract):
+		"""
+		# single identifier
+		if isinstance(toExtract, int):
+			toExtract = [toExtract]	
+		# list of identifiers
+		if isinstance(toExtract, list):
+			if number is None:
+				number = len(toExtract)
+			# if randomize, use random sample
+			if randomize:
+				toExtract = random.sample(toExtract, number)
+			# else take the first number members of toExtract
+			else:
+				toExtract = toExtract[:number]
+			# convert IDs if necessary
+			toExtractIndices = []
+			for value in toExtract:
+				toExtractIndices.append(self._getIndex(value))
+			return self._extractColumnsByList_implementation(toExtractIndices)
+		# boolean function
+		if hasattr(toExtract, '__call__'):
+			if randomize:
+				#apply to each
+				raise NotImplementedError # TODO randomize in the extractRowByFunction case
+			else:
+				if number is None:
+					number = self.rows()		
+				return self._extractColumnsByFunction_implementation(toExtract, number)
+		# by range
+		if start is not None or end is not None:
+			if start is None:
+				start = 0
+			if end is None:
+				end = self.rows()
+			if number is None:
+				number = end - start
+			if randomize:
+				return self.extactColumnsByList(random.randrange(start,end,number))
+			else:
+				return self._extractColumnsByRange_implementation(start, end)
+
+
+	def _extractColumnsByList_implementation(self,toExtract):
 		"""
 		
 
