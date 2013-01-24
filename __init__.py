d8faf244bceb90014daae25b1c8457e57946ddd7
Trevor Burns <trevorpburns@gmail.com>
2013-01-24 12:23:42 -0500

    moved some of cross validation up to root, modified the package inits to allow easier access to the rest

--- __init__.py
+++ __init__.py
@@ -1,5 +1,10 @@
 from umlExtensible import run
 from umlExtensible import data
+
 from umlStatic import normalize
-from umlStatic import crossValidate
+from umlStatic import loadTrainingAndTesting
+
+from combinatoins import crossValidate
+from combinatoins import crossValidateReturnBest
+from combinations import functionCombinations
 
---
+++ combinations/__init__.py
@@ -0,0 +1,5 @@
+from CrossValidate import crossValidate
+from CrossValidate import crossValidateReturnBest
+
+from Combinations import functionCombinations
+
