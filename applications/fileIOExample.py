9527397780a573efa9479c219c1cae278b15955e
Trevor Burns <trevorpburns@gmail.com>
2013-01-31 16:40:23 -0500

    added example showing both file input and output

---
+++ applications/fileIOExample.py
@@ -0,0 +1,41 @@
+"""
+Script loading some data, spliting it, and writing the results to seperate files
+
+"""
+
+from allowImports import boilerplate
+boilerplate()
+
+
+if __name__ == "__main__":
+
+	import sys
+	from UML import normalize
+	from UML import data
+
+	# string manipulation to get and make paths
+	pathOrig = sys.argv[1]
+	pathSplit = pathOrig.rsplit('.',1)
+	if len(pathSplit) > 1:
+		suffix = '.' + pathSplit[1]
+	else:
+		suffix = ""
+	pathTrain = pathSplit[0] + "TRAIN" + suffix
+	pathTest = pathSplit[0] + "TEST" + suffix
+
+	# we specify that we want a DenseMatrixData object returned, and with just the path it will
+	# decide automaticallly the format of the file that is being loaded
+	train = data("DenseMatrixData", pathOrig)
+
+	#scrub the set of any string valued data
+	train.dropStringValuedFeatures()
+
+	# split off a random test set
+	total = train.points()
+	num = int(round(.15*total))
+	test = train.extractPoints(start=0, end=total, number=num, randomize=True)
+
+	# output the split and normalized sets for later usage
+	train.writeFile('csv', pathTrain, includeFeatureNames=False)
+	test.writeFile('csv', pathTest, includeFeatureNames=False)
+
