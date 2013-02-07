654ffe484882756837ad23c85f295112a3a30512
Spencer <willfind@gmail.com>
2013-02-07 18:32:56 -0500

    changed which file is being used

--- applications/fileIOExample.py
+++ applications/fileIOExample.py
@@ -12,7 +12,7 @@ if __name__ == "__main__":
 	from UML import data
 
 	# string manipulation to get and make paths
-	pathOrig = "example_data/adult_income_classification_orig.csv"
+	pathOrig = "example_data/adult_income_classification_tiny.csv"
 	pathSplit = pathOrig.rsplit('.',1)
 	if len(pathSplit) > 1:
 		suffix = '.' + pathSplit[1]
@@ -21,7 +21,7 @@ if __name__ == "__main__":
 	pathTrain = pathSplit[0] + "TRAIN" + suffix
 	pathTest = pathSplit[0] + "TEST" + suffix
 
-	# we specify that we want a DenseMatrixData object returned, and with just the path it will
+	# we specify that we want a RowListData object returned, and with just the path it will
 	# decide automaticallly the format of the file that is being loaded
 	train = data("RowListData", pathOrig)
 
