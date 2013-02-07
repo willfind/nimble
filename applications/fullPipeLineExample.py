af7fe06f2ccd6c5984f6a441b2c65a3c4567be44
ross <ross.noren@gmail.com>
2013-02-07 18:27:42 -0500

    fixed sample/dummy data; now properly coordinated with arguments in run function

--- applications/fullPipeLineExample.py
+++ applications/fullPipeLineExample.py
@@ -1,6 +1,5 @@
 """
 Short module demonstrating the full pipeline of train - test - log results.
-
 """
 
 from allowImports import boilerplate
@@ -14,17 +13,17 @@ if __name__ == "__main__":
 	from UML.performance.metric_functions import classificationError
 
 	variables = ["x1","x2","x3"]
-	data1 = [[1,0,0,1], [3,3,3,1], [5,0,0,0],]
+	data1 = [[1,0,0], [3,3,3], [5,0,0],]
 	trainObj = data('DenseMatrixData', data1, variables)
 
-	data2 = [[1,0,0,1],[1,1,1,0],[5,1,1,0], [3,4,4,1]]
+	data2 = [[1,0,0],[1,1,1],[5,1,1], [3,4,4]]
 	testObj = data('DenseMatrixData', data2)
 
-	runnerFuncStr = """def logisticRegr(trainX, testX, dependentVar):
-		return run('sciKitLearn.LogisticRegression', trainX, testX, dependentVar, False)
+	runnerFuncStr = """def sciKitKMeans(trainX, testX, dependentVar):
+		return run('sciKitLearn.KMeans', trainX, testX, None, dependentVar, arguments={'n_clusters':3}, sendToLog=False)
 		"""
 
 	metricFuncs = []
 	metricFuncs.append(classificationError)
 
-	results = runAndTest(trainObj, testObj, trainDependentVar=3, testDependentVar=3, function=runnerFuncStr, performanceMetricFuncs=metricFuncs, sendToLog=True)
+	results = runAndTest(trainObj, testObj, trainDependentVar=2, testDependentVar=2, function=runnerFuncStr, performanceMetricFuncs=metricFuncs, sendToLog=True)
