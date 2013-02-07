09d1a43327b4d87d70d3209f11e41fce49659fbb
Trevor Burns <trevorpburns@gmail.com>
2013-02-07 15:37:40 -0500

    crossvalidate example changed to use mlpy

--- applications/crossValidateExample.py
+++ applications/crossValidateExample.py
@@ -10,8 +10,6 @@ if __name__ == "__main__":
 	from UML import loadTrainingAndTesting
 	from UML import functionCombinations
 	from UML import runAndTestDirect
-	from UML import runAndTest
-	from UML import run
 	from UML.performance.metric_functions import classificationError
 
 	# path to input specified by command line argument
@@ -19,19 +17,9 @@ if __name__ == "__main__":
 	trainX, trainY, testX, testY = loadTrainingAndTesting(pathIn, labelID='income', fractionForTestSet=.15, loadType="DenseMatrixData", fileType="csv")
 
 	# run and test with a direct call to run()
-	toRun = 'runAndTestDirect("sciKitLearn.LogisticRegression", trainX, testX, trainY, testY, {"C":<1|.05|.01|.005|.001>}, [classificationError])'
+	toRun = 'runAndTestDirect("mlpy.KNN", trainX, testX, trainY, testY, {"k":<1|5|10|15>}, [classificationError])'
 	runs = functionCombinations(toRun)
 	extraParams = {'runAndTestDirect':runAndTestDirect, 'classificationError':classificationError}
-
-	bestFunction, performance = crossValidateReturnBest(trainX, trainY, runs, mode='min', numFolds=10, extraParams=extraParams)
-	print bestFunction
-	print performance
-
-	# run and test with a constructed call to run()
-	runCall = '"run(\'sciKitLearn.LogisticRegression\', trainX, testX, dependentVar=dependentVar, arguments={\'C\':<1|.05|.01|.005|.001>})"'
-	toRun = 'runAndTest(trainX, testX, trainY, testY,' + runCall +',[classificationError])'
-	runs = functionCombinations(toRun)
-	extraParams = {'runAndTest':runAndTest, 'classificationError':classificationError, 'run':run}
 
 	bestFunction, performance = crossValidateReturnBest(trainX, trainY, runs, mode='min', numFolds=10, extraParams=extraParams)
 	print bestFunction
