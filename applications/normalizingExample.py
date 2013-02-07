fe8078958939e751f832e1ffe742b670214c93ba
Trevor Burns <trevorpburns@gmail.com>
2013-02-07 18:55:14 -0500

    modified the normalizing example to call only mlpy algorithms

--- applications/normalizingExample.py
+++ applications/normalizingExample.py
@@ -12,20 +12,32 @@ if __name__ == "__main__":
 	from UML import normalize
 	from UML import data
 
-	variables = ["x1","x2","x3"]
-	data1 = [[1,0,0], [3,3,3], [5,0,0],]
+	# we separate into classes accoring to whether x1 is positive or negative
+	variables = ["y","x1","x2","x3"]
+	data1 = [[1,6,0,0], [1,3,0,0], [0,-5,0,0],[0,-3,0,0]]
 	trainObj = data('DenseMatrixData', data1, variables)
+	trainObjY = trainObj.extractFeatures('y')
 
-	data2 = [[1,0,0],[1,1,1],[5,1,1], [3,4,4]]
+	# data we're going to classify
+	data2 = [[1,0,0],[4,0,0],[-1,0,0], [-2,0,0]]
 	testObj = data('DenseMatrixData', data2)
 
-	normalize('mlpy.PCA', trainObj, testObj, arguments={'k':2})
-	ret = run('sciKitLearn.KMeans', trainObj, testObj, arguments={'n_clusters':3})
+	# baseline check
+	assert trainObj.data[0].size == 3
+	assert testObj.data[0].size == 3
 
-	# clustering returns a row vector of indices, referring to the cluster centers,
-	# we don't care about the exact numbers, this verifies that the appropriate
-	# ones are assigned to the same clusters
-	assert ret.data[0,0] == ret.data[1,0]
-	assert ret.data[0,0] != ret.data[2,0]
-	assert ret.data[0,0] != ret.data[3,0]
-	assert ret.data[2,0] != ret.data[3,0]
+	# use normalize to modify our data; we call a dimentionality reduction algorithm to
+	# simply our mostly redundant points. k is the desired number of dimensions in the output
+	normalize('mlpy.PCA', trainObj, testObj, arguments={'k':1})
+
+	# assert that we actually do have fewer dimensions
+	assert trainObj.data[0].size == 1
+	assert testObj.data[0].size == 1
+
+	ret = run('mlpy.KNN', trainObj, testObj, dependentVar=trainObjY, arguments={'k':1})
+
+	# assert we get the correct classes 
+	assert ret.data[0,0] == 1
+	assert ret.data[1,0] == 1
+	assert ret.data[2,0] == 0
+	assert ret.data[3,0] == 0
