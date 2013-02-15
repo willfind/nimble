de99c69484ef867c48e96bd2dc36d36e55d8e10b
ross <ross.noren@gmail.com>
2013-02-14 20:14:31 -0500

    Fixed bad init() function: changed from startNames = dict() to self.startNames = dict.  Added unit test

--- logging/stopwatch.py
+++ logging/stopwatch.py
@@ -10,35 +10,49 @@ import time
 class Stopwatch(object):
 
 	def __init__(self):
-		startTimes = {}
-		stopTimes = {}
+		self.startTimes = dict()
+		self.stopTimes = dict()
 
 	def start(self, taskName):
-	"""
-		Record the start time for the provided taskName.  If there is already
-		a start time associated with taskName, overwrite the old start time.
-		TODO: May need to change that to raising an exception, instead of overwriting).
-	"""
+		"""
+			Record the start time for the provided taskName.  If there is already
+			a start time associated with taskName, overwrite the old start time.
+			TODO: May need to change that to raising an exception, instead of overwriting).
+		"""
 		self.startTimes[taskName] = time.clock()
 
 	def stop(self, taskName):
-	"""
-		Record the stop time for the provided taskName.  If there is already a stop time
-		associated with taskName, overwrite the old stop time.
-		TODO: Possibly raise an exception instead of overwriting existing stop time, if there
-		is already an entry for taskName.
-	"""
+		"""
+			Record the stop time for the provided taskName.  If there is already a stop time
+			associated with taskName, overwrite the old stop time.
+			TODO: Possibly raise an exception instead of overwriting existing stop time, if there
+			is already an entry for taskName.
+		"""
 		self.stopTimes[taskName] = time.clock()
 
+	def continueTiming(self, taskName):
+		"""
+			If a task has previously been started and
+		"""
+
 	def calcRunTime(self, taskName):
-	"""
-		Calculate the time it took for the task associated with taskName to complete,
-		based on the stored start time and stop time.  If either the start time or stop
-		time for taskName is empty, returns None.
-		TODO: instead of returning None, raise an exception if start time or stop time is
-		unavailable.
-	"""
+		"""
+			Calculate the time it took for the task associated with taskName to complete,
+			based on the stored start time and stop time.  If either the start time or stop
+			time for taskName is empty, returns None.
+			TODO: instead of returning None, raise an exception if start time or stop time is
+			unavailable.
+		"""
 		if self.startTimes[taskName] is None or self.stopTimes[taskName] is None:
 			return None
 		else:
 			return self.stopTimes[taskName] - self.startTimes[taskName]
+
+
+def testBasicFuncs():
+	watch = Stopwatch()
+	watch.start('test')
+	watch.stop('test')
+	runTime = watch.calcRunTime('test')
+	assert runTime > 0.0
+	assert runTime < 10.0
