f3b82106aabed175c2893e3766a06b6633226276
ross <ross.noren@gmail.com>
2013-02-18 13:05:55 -0500

    fixed imports of exceptions - added additional period to path, added more unit tests, added another dictionary to track which tasks are running

--- logging/stopwatch.py
+++ logging/stopwatch.py
@@ -5,7 +5,9 @@
 """
 
 import time
-from .utility.custom_exceptions import MissingEntryException
+import nose
+from ..utility.custom_exceptions import MissingEntryException
+from ..utility.custom_exceptions import ImproperActionException
 
 class Stopwatch(object):
 
@@ -13,7 +15,7 @@ class Stopwatch(object):
 		self.startTimes = dict()
 		self.stopTimes = dict()
 		self.cumulativeTimes = dict()
-		self.isRunning = dict()
+		self.isRunningStatus = dict()
 
 	def start(self, taskName):
 		"""
@@ -21,8 +23,11 @@ class Stopwatch(object):
 			a start time associated with taskName, overwrite the old start time.
 			TODO: May need to change that to raising an exception, instead of overwriting).
 		"""
-		self.startTimes[taskName] = time.clock()
-		del self.startTimes[taskName]
+		if taskName in self.isRunningStatus and self.isRunningStatus[taskName] == True:
+			raise ImproperActionException("Task: " + taskName + " has already been started.")
+		else:
+			self.startTimes[taskName] = time.clock()
+			self.isRunningStatus[taskName] = True
 
 	def stop(self, taskName):
 		"""
@@ -31,30 +36,63 @@ class Stopwatch(object):
 			TODO: Possibly raise an exception instead of overwriting existing stop time, if there
 			is already an entry for taskName.
 		"""
+		if taskName not in self.startTimes or taskName not in self.isRunningStatus:
+			raise MissingEntryException([taskName], "Tried to stop task that was not started in Stopwatch.stop()")
+		elif not self.isRunningStatus[taskName]:
+			raise ImproperActionException("Unable to stop task that has already stopped")
+
 		self.stopTimes[taskName] = time.clock()
-		if self.startTimes[taskName] is not None:
-			if self.cumulativeTimes[taskName] is None:
-				self.cumulativeTimes[taskName] += self.stopTimes[taskName] - self.startTimes[taskName]
-			else:
-				self.cumulativeTimes[taskName] = self.stopTimes[taskName] - self.startTimes[taskName]
+		self.isRunningStatus[taskName] = False
+		if taskName in self.cumulativeTimes:
+			self.cumulativeTimes[taskName] += self.stopTimes[taskName] - self.startTimes[taskName]
+		else:
+			self.cumulativeTimes[taskName] = self.stopTimes[taskName] - self.startTimes[taskName]
+
+	def reset(self, taskName, resetCumulativeTime=True):
+		"""
+			Reset/remove all values in stopwatch associated with taskName.  If resetCumulativeTime is
+			True, get rid of the running total of time for taskName.  If resetCumulativeTime is false,
+			the running total is preserved but taskName will be stopped and isRunning(taskName) will
+			return False.
+		"""
+		if taskName in self.startTimes:
+			del self.startTimes[taskName]
+
+		if taskName in self.stopTimes:
+			del self.stopTimes[taskName]
+
+		if taskName in self.isRunningStatus:
+			self.isRunningStatus[taskName] = False
+
+		if resetCumulativeTime:
+			self.cumulativeTimes[taskName] = 0.0
 
 	def isRunning(self, taskName):
 		"""
 			Check if the stopwatch is currently timing a task with taskName (i.e. the timer has been
-			started but not stopped for said task)
+			started but not stopped for said task).  Returns true if start() has been called for
+			this taskName but neither stop() nor reset() have been called for this taskName.  Otherwise
+			returns false.
 		"""
+		if taskName in self.isRunningStatus:
+			if self.isRunningStatus[taskName] == True:
+				return True
+		else:
+			return False
 
 	def calcRunTime(self, taskName):
 		"""
 			Calculate the time it took for the task associated with taskName to complete,
-			based on the stored start time and stop time.  If either the start time or stop
-			time for taskName is empty, returns None.
-			TODO: instead of returning None, raise an exception if start time or stop time is
-			unavailable.
+			based on the stored start time and stop time.  If the timer is still timing the
+			task associated with taskName (i.e. isRunning(taskName) is True), raises an
+			ImproperActionException.
 		"""
-		if self.cumulativeTimes[taskName] is None:
+		if taskName not in self.cumulativeTimes or taskName not in self.isRunningStatus:
 			raise MissingEntryException([taskName], "Missing entry when trying to calculate total task run time")
-		else return self.cumulativeTimes[taskName]
+		elif self.isRunningStatus[taskName] == True:
+			raise ImproperActionException('Can\'t calculate total running time for ' + taskName + ', as it is still running')
+		else:
+			return self.cumulativeTimes[taskName]
 
 
 
@@ -71,4 +109,17 @@ def testBasicFuncs():
 	watch.stop('test')
 	watch.startTimes['test'] = 0.00
 	watch.stopTimes['test'] = 5.00
+	watch.cumulativeTimes['test'] = 5.00
+
+	runTime = watch.calcRunTime('test')
+	assert runTime == 5.0
+	assert watch.isRunning('test') == False
+
+	watch.start('test')
+	assert watch.isRunning('test') == True
+
+	nose.assert_raises(ImproperActionException, watch.start, ('test'))
+	nose.assert_raises(MissingEntryException, watch.calcRunTime, ('uncle'))
+	nose.assert_raises(ImproperActionException, watch.calcRunTime, ('test'))
+	nose.assert_raises(MissingEntryException, watch.stop, ('uncle'))
 
