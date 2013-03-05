16da9f1f9929ffecc3c365dfc43ed7756e175aac
ross <ross.noren@gmail.com>
2013-03-05 12:11:46 -0500

    Further updates to data_set_analyzer: not debugged

--- logging/data_set_analyzer.py
+++ logging/data_set_analyzer.py
@@ -6,193 +6,354 @@ entries), min, max, mean, st. dev., # of unique values (for non-real valued feat
 missing values
 """
 import math
+import numpy as np
 
+from UML import data
 from UML.processing.base_data import BaseData
 from UML.processing.row_list_data import RowListData
 from UML.logging.tableString import tableString
+from UML.utility import ArgumentException
 
 
-class DataSetAnalyzer(object):
 
-	def __init__(self, dataSet=None):
-		"""
-		Store the dataSet associated with this object.  dataSet should be an object of
-		a type that inherits from the BaseData class.
-		"""
-		if dataSet is not None:
-			self.dataSet = dataSet
+def featurewiseFunctionGenerator(self):
+	"""
+	TODO: fill out docstring
+	"""
+	functions = [min_, max_, mean_, median_, standardDeviation, numUnique, featureType, numValues]
+	return functions
 
-	def setData(self, dataSet):
-		"""
-		Setter for this object's dataSet field.
-		"""
-		self.dataSet = dataSet
+def produceFeatureWiseReport(dataContainer, supplementalFunctions=None, maxFeaturesToCover=50):
+	"""
+	Produce a report with summary information about each feature in the
+	data set.  Returns the report as a string containing a 2-dimensional 
+	table, with each row representing a feature and each column representing
+	a piece of information about the feature.  If the number of features in
+	the data set is greater than maxFeaturesToCover, only a subset (equal in size
+	to maxFeaturesToCover) of features will be included in the report.
+	Input:
+		supplementalFunctions:  list of supplemental functions to be applied to each feature, 
+		which must accept a 1D vector and return a scalar value.  These will be applied
+		in addition to the default functions (min, max, mean, st. dev, # unique values,
+		# missing values, # zero values, etc.).
 
-	def featurewiseFunctionGenerator(self):
-		"""
-		TODO: fill out body of function, which should return a list of default 
-		functions that should be applied to each feature
-		"""
-		functions = [self.min_, self.max_, self.mean_, self.standardDeviation, self.numUnique]
-		return functions
+		maxFeaturesToCover: if the number of features exceeds maxFeaturesToCover, then
+		summary information will only be calculated and reported for a subset of features, 
+		equal in size to maxFeaturesToCover.
 
-	def produceFeatureWiseReport(self, supplementalFunctions=None, maxFeaturesToCover=50):
-		"""
-		Produce a report with summary information about each feature in the
-		data set.  Returns the report as a string containing a 2-dimensional 
-		table, with each row representing a feature and each column representing
-		a piece of information about the feature.  If the number of features in
-		the data set is greater than maxFeaturesToCover, only a subset (equal in size
-		to maxFeaturesToCover) of features will be included in the report.
-		Input:
-			supplementalFunctions:  list of supplemental functions to be applied to each feature, 
-			which must accept a 1D vector and return a scalar value.  These will be applied
-			in addition to the default functions (min, max, mean, st. dev, # unique values,
-			# missing values, # zero values, etc.).
+	Output:
+		A string containing a table with each row containing summary information about one
+		of the features in the data set.  Each column represents the results of one of
+		the functions applied to each feature.
+	"""
+	functionsToApply = featurewiseFunctionGenerator()
+	if supplementalFunctions is not None:
+		functionsToApply.extend(supplementalFunctions)
 
-			maxFeaturesToCover: if the number of features exceeds maxFeaturesToCover, then
-			summary information will only be calculated and reported for a subset of features, 
-			equal in size to maxFeaturesToCover.
+	columnLabels = ['featureName']
+	for func in functionsToApply:
+		label = func.__name__
+		columnLabels.append(label)
 
-		Output:
-			A string containing a table with each row containing summary information about one
-			of the features in the data set.  Each column represents the results of one of
-			the functions applied to each feature.
-		"""
-		functionsToApply = self.featurewiseFunctionGenerator()
-		if supplementalFunctions is not None:
-			functionsToApply.extend(supplementalFunctions)
+    #try to find out basic information about the data set: # of rows
+    #and columns.  For numpy containers, this is obtained through call
+    #to shape().  For RowListData, can use len()
+    try:
+        numRows = dataContainer.data.shape[0]
+        numColumns = dataContainer.data.shape[1]
+    except AttributeError: 
+        numRows = len(dataContainer.data[0])
+        numColumns = len(dataContainer.data)
 
-		columnLabels = []
-		for func in functionsToApply:
-			label = func.__name__
-			columnLabels.append(label)
+    #If the data object is too big to print out info about each feature,
+    #get a 
+    if numColumns > maxFeaturesToCover:
+        leftFeatureIndices = range(maxFeaturesToCover/2)
+        leftSlice = dataContainer.copyFeatures(leftFeatureIndices)
 
-		rowLabelsList = [None] * len(self.dataSet.featureNames)
-		for featureName, index in self.dataSet.featureNames.iteritems():
-			rowLabelsList[index] = featureName
+        rightFeatureIndices = 
 
-		for rowLabel in rowLabelsList:
-			print rowLabel
+	rowLabelsList = [None] * len(dataSet.featureNames)
+	for featureName, index in dataSet.featureNames.iteritems():
+		rowLabelsList[index] = [featureName]
 
-		featurewiseResults = RowListData(rowLabelsList)
-		#print len(featurewiseResults.data)
-		featurewiseResults.transpose()
-		print tableString(featurewiseResults.data, False)
-		for func in functionsToApply:
-			oneFuncResults = self.dataSet.applyFunctionToEachFeature(func)
-			#print len(oneFuncResults.data)
-			oneFuncResults.transpose()
-			#print len(oneFuncResults.data)
-			oneFuncResults.renameFeatureName(0, func.__name__)
-			oneFuncResults = oneFuncResults.toRowListData()
-			featurewiseResults.appendFeatures(oneFuncResults)
+	for func in functionsToApply:
+		oneFuncResults = dataSet.applyFunctionToEachFeature(func)
+		oneFuncResults.transpose()
+		oneFuncResultsList = oneFuncResults.toListOfLists()
+		appendColumns(rowLabelsList, oneFuncResultsList)
 
-		table = tableString(featurewiseResults.data, True, columnLabels)
+    #In the case that this is a sparse matrix, we need to adjust the statistics
+    #to account for missing zeros
+    if rowLabelsList[1][-1] != numRows:
+        for row in rowLabelsList[1:]:
+            row = adjustStats(row, rowLabelsList[0], numRows)
 
-		return table
+	return table
+
+def convertToPrintableTable(table, headers):
+    """
+        TODO: add docstring
+    """
+    return tableString(table, True, headers)
+
+def adjustStats(rawStats, funcNames, totalNumRows):
+    """
+        TODO: add docstring
+    """
+    rawNumRows = rawStats[-1]
+
+    #find value of 'mean' for this feature
+    for i in range(rawStats):
+        if funcNames[i] == 'mean_':
+            mean = rawStats[i]
+        else:
+            pass
+
+    for i in range(rawStats):
+        rawStat = rawStats[i]
+        funcName = funcNames[i]
+        if funcName == "min_":
+            if rawStat > 0:
+                rawStats[i] = 0
+            else:
+                rawStats[i] = rawStat
+        elif funcName == "max_":
+            if rawStat < 0:
+                rawStats[i] = 0
+            else:
+                rawStats[i] = rawStat
+        elif funcName == "median_":
+
+        elif funcName == "mean_":
+            rawStats[i] = = float(rawStat * rawNumRows) / float(totalNumRows)
+        elif funcName == "standardDeviation":
+            rawStats[i] = math.sqrt(rawStats[i]**2 + (totalNumRows - rawNumRows)*(mean**2))
+        elif funcName == "numUnique":
+            rawStats[i] = rawStat + 1
+        elif funcName == 'featureType':
+            rawStats[i] = rawStat
+        else:
+            rawStats[i] = rawStat
+
+    return rawStats
 
 
-	def isNumerical(self, vector):
-		"""
-		Returns true if the vector contains primitive numerical non-complex values, 
-		returns false otherwise.  Assumes that all items in vector are of the same type.
-		"""
-		for item in vector:
-			if isinstance(item, (int, float, long)):
-				return True
-			else:
-				return False
+def appendColumns(appendTo, appendFrom):
+	"""
+		Append the columns of one 2D matrix (lists of lists) into another.  
+		They must have the same number of rows, but can have different numbers 
+		of columns.  I.e. len(appendTo) == len(appendFrom), but 
+		len(appendTo[0]) == len(appendFrom[0]) does not need to be true.
+		If they do not have the same number of rows, an ArgumentException is
+		raised.
 
+	"""
+	if len(appendTo) != len(appendFrom):
+		raise ArgumentException("Can't merge two matrices with different numbers of rows: " + 
+			len(appendTo) + " != " + len(appendFrom))
 
-	def min_(self, values):
-		"""
-		Given a 1D vector of values, find the minimum value.  If the values are
-		not numerical, return None.
-		"""
-		if not self.isNumerical(values):
-			return None
+	for i in xrange(len(appendTo)):
+		appendFromRow = appendFrom[i]
+		for j in xrange(len(appendFromRow)):
+			appendTo[i].append(appendFromRow[j])
 
-		currMin = float("inf")
+	return
 
-		for value in values:
-			if not math.isnan(value) and value < currMin:
-				currMin = value
-
-		if currMin != float("-inf"):
-			return currMin
+def isNumericalFeatureGuesser(featureVector):
+	"""
+	Returns true if the vector contains primitive numerical non-complex values, 
+	returns false otherwise.  Assumes that all items in vector are of the same type.
+	"""
+	for item in featureVector:
+		if isinstance(item, (int, float, long)):
+			return True
+		elif item is None or math.isnan(item):
+			pass
 		else:
-			return None
+			return False
 
-	def max_(self, values):
-		"""
-		Given a 1D vector of values, find the maximum value.  If the values are
-		not numerical, return None.
-		"""
-		if not self.isNumerical(values):
-			return None
+	return True
 
-		currMax = float("-inf")
+def isNumericalPoint(point):
+	"""
+	Check to see if a point is a valid number that can be used in numerical calculations.
+	If point is of type float, long, or int, and not None or NaN, return True.  Otherwise
+	return False.
+	"""
+	if isinstance(point, (int, float, long)) and not math.isnan(point):
+		return True
+	else:
+		return False
 
-		for value in values:
-			if not math.isnan(value) and value < currMax:
-				currMax = value
 
-		if currMax != float("-inf"):
-			return currMax
-		else:
-			return None
+def min_(values):
+	"""
+	Given a 1D vector of values, find the minimum value.  If the values are
+	not numerical, return None.
+	"""
+	if not isNumericalFeatureGuesser(values):
+		return None
 
-	def mean_(self, values):
-		"""
-		Given a 1D vector of values, find the minimum value.  If the values are
-		not numerical, return None.
-		"""
-		if not self.isNumerical(values):
-			return None
+	currMin = float("inf")
+	count = 0
 
-		count = 0
-		runningSum = 0
+	for value in values:
+		count += 1
+		if isNumericalPoint(value) and value < currMin:
+			currMin = value
 
-		for value in values:
-			if not math.isnan(value):
-				runningSum += value
-				count += 1
+	if not math.isinf(currMin):
+		return currMin
+	else:
+		return None
 
-		if count > 0:
-			return float(runningSum) / count
+def max_(values):
+	"""
+	Given a 1D vector of values, find the maximum value.  If the values are
+	not numerical, return None.
+	"""
+	if not isNumericalFeatureGuesser(values):
+		return None
 
-	def standardDeviation(self, values):
-		"""
-		Given a 1D vector of values, find the standard deviation.  If the values are
-		not numerical, return None.
-		"""
-		if not self.isNumerical(values):
-			return None
+	currMax = float("-inf")
 
-		count = 0
-		runningSum = 0
+	count = 0
 
-		for value in values:
-			if not math.isnan(value):
-				runningSum += value
-				count += 1
+	for value in values:
+		count += 1
+		if isNumericalPoint(value) and value > currMax:
+			currMax = value
 
-		if not count > 0:
-			return 0
-		else:
-			mean = float(runningSum)/count
+	if not math.isinf(currMax):
+		return currMax
+	else:
+		return None
 
-		squaredDifferenceTotal = 0
-		for value in values:
+def mean_(values):
+	"""
+	Given a 1D vector of values, find the minimum value.  If the values are
+	not numerical, return None.
+	"""
+	if not isNumericalFeatureGuesser(values):
+		return None
+
+	totalCount = 0
+	count = 0
+	runningSum = 0
+
+	for value in values:
+		totalCount += 1
+		if isNumericalPoint(value):
+			runningSum += value
+			count += 1
+
+	if count > 0:
+		return float(runningSum) / count, totalCount
+	else:
+		return None
+
+def median_(values):
+	"""
+	Given a 1D vector of values, find the minimum value.  If the values are
+	not numerical, return None.
+	TODO: handle nan/None values
+	"""
+	if not isNumericalFeatureGuesser(values):
+		return None
+
+	count = 0
+	for value in values:
+		count += 1
+
+	#Filter out None/NaN values from list of values
+	sortedValues = filter(lambda x: not (x == None or math.isnan(x)), values)
+
+	sortedValues = sorted(values)
+
+	numValues = len(sortedValues)
+
+	if numValues % 2 == 0:
+		median = float(sortedValues[numValues/2])
+	else:
+		median = (sortedValues[(numValues + 1) / 2] + sortedValues[(numValues - 1) / 2]) / float(2)
+
+	return median
+
+def standardDeviation(values):
+	"""
+	Given a 1D vector of values, find the standard deviation.  If the values are
+	not numerical, return None.
+	"""
+	if not isNumericalFeatureGuesser(values):
+		return None
+
+	#Filter out None/NaN values from list of values
+	mean = mean_(values)[0]
+	count = 0
+
+	squaredDifferenceTotal = 0
+	for value in values:
+		count += 1
+		if isNumericalPoint(value):
 			squaredDifferenceTotal += (mean - value)**2
 
-		stDev = math.sqrt(squaredDifferenceTotal / count)
-		return stDev
+	stDev = math.sqrt(squaredDifferenceTotal / float(count))
+	return stDev
 
-	def numUnique(self, values):
-		"""
-		Given a 1D vector of values, calculate the number of unique values.
-		"""
-		valueSet = set(values)
-		return len(valueSet)
+def numUnique(values):
+	"""
+	Given a 1D vector of values, calculate the number of unique values.
+	"""
+	values = filter(lambda x: not (x == None or math.isnan(x)), values)
+	valueSet = set(values)
+	return len(valueSet)
+
+def featureType(values):
+    """
+        Return the type of data: string, int, float
+        TODO: add numpy type checking
+    """
+    for value in values:
+        if isinstance(value, str):
+            return "string"
+        elif isinstance(value, (int, long))
+            return "int"
+        elif isinstance(value, (float)):
+            return "float"
+        elif isinstance(value, complex):
+            return "complex"
+        else:
+            pass
+
+    return "Unknown"
+
+
+def numValues(values):
+    """
+        Compute the number of values in the feature, whether they are strings,
+        numerical, None, or NaN.
+    """
+    count = 0
+    for item in values:
+        count += 1
+
+    return count
+
+
+def testDense():
+    data1 = np.array([1, 2, 0, 1], [3, 3, 1, 0], [0, 0, 5, 2])
+    names1 = ['var1', 'var2', 'var3', 'var4']
+
+    trainObj = data('DenseMatrixData', data1, names1)
+    rawTable = produceFeatureWiseReport(trainObj)
+    funcNames = rawTable[0]
+    funcIndex = 0
+    for 
+
+
+def testSparse():
+    pass
+
+def testRowList():
+    pass
