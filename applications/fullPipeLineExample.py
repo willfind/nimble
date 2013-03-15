bb774b981cfd806f8f09830778d26afae0406d88
ross <ross.noren@gmail.com>
2013-03-15 14:56:18 -0400

    changed fullPipeLineExample to use runOneVsOne

--- applications/fullPipeLineExample.py
+++ applications/fullPipeLineExample.py
@@ -6,20 +6,25 @@ from allowImports import boilerplate
 boilerplate()
 
 if __name__ == "__main__":
-	from UML import run
-	from UML import normalize
 	from UML import data
 	from UML.performance.runner import runAndTest
+	from UML.performance.runner import runOneVsOne
 	from UML.performance.metric_functions import classificationError
 
-	variables = ["x1","x2","x3"]
-	data1 = [[1,0,0], [3,3,3], [5,0,0],]
+	variables = ["x1","x2","x3", "label"]
+	data1 = [[1,0,0,1], [0,1,0,2], [0,0,1,3], [1,0,0,1], [0,1,0,2], [0,0,1,3], 
+	[1,0,0,1], [0,1,0,2], [0,0,1,3], [1,0,0,1], [0,1,0,2], [0,0,1,3], [1,0,0,1], [0,1,0,2], [0,0,1,3], [1,0,0,3], [0,1,0,1], [0,0,1,2]]
 	trainObj = data('DenseMatrixData', data1, variables)
 
-	data2 = [[1,0,0],[1,1,1],[5,1,1], [3,4,4]]
+	data2 = [[1,0,0,1],[0,1,0,2],[0,0,1,3]]
 	testObj = data('DenseMatrixData', data2)
 
 	metricFuncs = []
 	metricFuncs.append(classificationError)
 
-	results = runAndTest('sciKitLearn.KMeans',trainObj, testObj, trainDependentVar=2, testDependentVar=2, arguments={'n_clusters':3}, performanceMetricFuncs=metricFuncs, sendToLog=True)
+	#results1 = runAndTest('sciKitLearn.LogisticRegression',trainObj, testObj, trainDependentVar=3, testDependentVar=3, arguments=None, performanceMetricFuncs=metricFuncs)
+	results2 = runOneVsOne('sciKitLearn.SVC',trainObj, testObj, trainDependentIndicator=3, testDependentIndicator=3, arguments={}, performanceMetricFuncs=metricFuncs)
+
+	#print 'Standard run results: '+str(results1)
+	print
+	print 'One vs One results: '+str(results2)
