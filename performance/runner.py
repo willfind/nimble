bebf055265a53a9633524b0fce1ebb695d776f50
ross <ross.noren@gmail.com>
2013-03-19 01:52:49 -0400

    added prototype for runAndTestOneVsAll() (can only be used when scores are available), debugging

--- performance/runner.py
+++ performance/runner.py
@@ -1,16 +1,15 @@
-import time
-from UML.combinations.Combinations import executeCode
-import performance_interface
+from UML.performance.performance_interface import computeMetrics
 from UML.processing import BaseData
 from UML.logging.log_manager import LogManager
 from UML.logging.stopwatch import Stopwatch
 from UML.interfaces.interface_helpers import generateAllPairs
+from UML.utility import ArgumentException
 from UML import run
 
 
 def runAndTest(algorithm, trainX, testX, trainDependentVar, testDependentVar, arguments, performanceMetricFuncs, sendToLog=True):
 	"""
-		Calls on run() to train and evaluate the learnin algorithm defined in 'algorithm,'
+		Calls on run() to train and evaluate the learning algorithm defined in 'algorithm,'
 		then tests its performance using the metric function(s) found in
 		performanceMetricFunctions
 
@@ -48,7 +47,7 @@ def runAndTest(algorithm, trainX, testX, trainDependentVar, testDependentVar, ar
 		timer.stop('train')
 
 	#now we need to compute performance metric(s) for all prediction sets
-	results = performance_interface.computeMetrics(testDependentVar, None, rawResult, performanceMetricFuncs)
+	results = computeMetrics(testDependentVar, None, rawResult, performanceMetricFuncs)
 
 	if sendToLog:
 		logManager = LogManager()
@@ -57,25 +56,56 @@ def runAndTest(algorithm, trainX, testX, trainDependentVar, testDependentVar, ar
 	return results
 
 
-def runOneVsOne(algorithm, trainX, testX, trainDependentIndicator, testDependentIndicator, arguments, performanceMetricFuncs, sendToLog=True):
+def runAndTestOneVsOne(algorithm, trainX, testX, trainDependentVar, testDependentVar=None, arguments={}, performanceMetricFuncs=None, sendToLog=True):
 	"""
-	TODO: add docstring
+	Calls on run() to train and evaluate the learning algorithm defined in 'algorithm.'  Assumes
+	there are multiple (>2) class labels, and uses the one vs. one method of splitting the 
+	training set into 2-label subsets. Tests performance using the metric function(s) found in 
+	performanceMetricFunctions.
+
+		trainX: data set to be used for training (as some form of BaseData object)
+		
+		testX: data set to be used for testing (as some form of BaseData object)
+		
+		trainDependentVar: used to retrieve the known class labels of the traing data. Either
+		contains the labels themselves (in a BaseData object of the same type as trainX) 
+		or an index (numerical or string) that defines their locale in the trainX object.
+		
+		testDependentVar: used to retreive the known class labels of the test data. Either
+		contains the labels themselves or an index (numerical or string) that defines their locale
+		in the testX object.  If not present, it is assumed that testDependentVar is the same
+		as trainDependentVar.  
+		
+		arguments: optional arguments to be passed to the function specified by 'algorithm'
+		
+		performanceMetricFuncs: iterable collection of functions that can take two collections
+		of corresponding labels - one of true labels, one of predicted labels - and return a
+		performance metric.
+		
+		sendToLog: optional boolean valued parameter; True meaning the results should be logged
 	"""
 
 	#TODO DUPLICATE DATA BEFORE CALLING RUN
 	trainX = trainX.duplicate()
 	testX = testX.duplicate()
 
-	# If testDependentIndicator is missing, assume it is because it's the same as trainDependentIndicator
-	if testDependentIndicator is None:
-		testDependentIndicator = trainDependentIndicator
+	if isinstance(trainDependentVar, BaseData):
+		trainX.appendFeatures(trainDependentVar)
+		trainDependentVar = trainX.features() - 1
+
+	# If testDependentVar is missing, assume it is because it's the same as trainDependentVar
+	if testDependentVar is None:
+		testDependentVar = trainDependentVar
 
 	#Remove true labels from testing set
-	testTrueLabels = testX.extractFeatures(testDependentIndicator)
+	if isinstance(testDependentVar, (str, int, long)):
+		testTrueLabels = testX.extractFeatures(testDependentVar)
+	else:
+		testTrueLabels = testDependentVar
 
 	# Get set of unique class labels, then generate list of all 2-combinations of
 	# class labels
-	labelVector = trainX.copyFeatures([trainDependentIndicator])
+	labelVector = trainX.copyFeatures([trainDependentVar])
 	labelVector.transpose()
 	labelSet = list(set(labelVector.toListOfLists()[0]))
 	labelPairs = generateAllPairs(labelSet)
@@ -91,10 +121,9 @@ def runOneVsOne(algorithm, trainX, testX, trainDependentIndicator, testDependent
 	rawPredictions = None
 	predictionFeatureID = 0
 	for pair in labelPairs:
-		filteringFunc = filterFuncGenerator(trainDependentIndicator, pair)
 		#get all points that have one of the labels in pair
-		pairData = trainX.extractPoints(filteringFunc)
-		pairTrueLabels = pairData.extractFeatures(trainDependentIndicator)
+		pairData = trainX.extractPoints(lambda point: (point[trainDependentVar] == pair[0]) or (point[trainDependentVar] == pair[1]))
+		pairTrueLabels = pairData.extractFeatures(trainDependentVar)
 		#train classifier on that data; apply it to the test set
 		partialResults = run(algorithm, pairData, testX, output=None, dependentVar=pairTrueLabels, arguments=arguments, sendToLog=False)
 		#put predictions into table of predictions
@@ -107,14 +136,14 @@ def runOneVsOne(algorithm, trainX, testX, trainDependentIndicator, testDependent
 		trainX.appendPoints(pairData)
 		predictionFeatureID +=1
 
-	finalPredictions = rawPredictions.applyFunctionToEachPoint(extractWinningPrediction)
+	finalPredictions = rawPredictions.applyFunctionToEachPoint(extractWinningPredictionLabel)
 
 	#if we are logging this run, we need to stop the timer
 	if sendToLog:
 		timer.stop('train')
 
 	#now we need to compute performance metric(s) for the set of winning predictions
-	results = performance_interface.computeMetrics(testTrueLabels, None, finalPredictions, performanceMetricFuncs)
+	results = computeMetrics(testTrueLabels, None, finalPredictions, performanceMetricFuncs)
 
 	# Send this run to the log, if desired
 	if sendToLog:
@@ -123,54 +152,116 @@ def runOneVsOne(algorithm, trainX, testX, trainDependentIndicator, testDependent
 
 	return results
 
-	
-def filterFuncGenerator(featureIndicator, desiredLabelPair):
+def runAndTestOneVsAll(algorithm, trainX, testX, trainDependentVar, testDependentVar=None, arguments={}, performanceMetricFuncs=None, sendToLog=True):
 	"""
-		Generate a function, whose only argument is a point (vector), that will
-		return true if the value in point indicated by featureIndicator is equal
-		to one of the two values in desiredLabelPair.  Useful for extracting points
-		from a BaseData object based on class/prediction value.
+	Calls on run() to train and evaluate the learning algorithm defined in 'algorithm.'  Assumes
+	there are multiple (>2) class labels, and uses the one vs. all method of splitting the 
+	training set into 2-label subsets. Tests performance using the metric function(s) found in 
+	performanceMetricFunctions.
 
-		Inputs:
-			featureIndicator - either an int or a string, representing an index in a
-			point 
-
-			desiredLabelPair - pair of values (either as a tuple or a list).  If
-			the value found in point at the index indicated by featureIndicator is 
-			equal to either of these two values, the generated function will return
-			True.  Otherwise, it will return false.
-
-		Output:
-			function that checks for equality of a feature in a point, designated by
-			featureIndicator, and 2 possible values, found in desiredLabelPair
+		trainX: data set to be used for training (as some form of BaseData object)
+		
+		testX: data set to be used for testing (as some form of BaseData object)
+		
+		trainDependentVar: used to retrieve the known class labels of the traing data. Either
+		contains the labels themselves (in a BaseData object of the same type as trainX) 
+		or an index (numerical or string) that defines their locale in the trainX object.
+		
+		testDependentVar: used to retreive the known class labels of the test data. Either
+		contains the labels themselves or an index (numerical or string) that defines their locale
+		in the testX object.  If not present, it is assumed that testDependentVar is the same
+		as trainDependentVar.  
+		
+		arguments: optional arguments to be passed to the function specified by 'algorithm'
+		
+		performanceMetricFuncs: iterable collection of functions that can take two collections
+		of corresponding labels - one of true labels, one of predicted labels - and return a
+		performance metric.
+		
+		sendToLog: optional boolean valued parameter; True meaning the results should be logged
 	"""
-	desiredLabel1 = desiredLabelPair[0]
-	desiredLabel2 = desiredLabelPair[1]
 
-	def tmp(point):
-		if (point[featureIndicator] == desiredLabel1) or (point[featureIndicator] == desiredLabel2):
-			return True
-		else: return False
+	#TODO DUPLICATE DATA BEFORE CALLING RUN
+	trainX = trainX.duplicate()
+	testX = testX.duplicate()
 
-	return tmp
 
-def valueCounter(values):
-	"""
-	Given an iterable list of values, count how many times each individual value occurs
-	and return a dictionary containing value:valueCount for each value in values.
-	"""
-	valueCounts = {}
-	for value in values:
-		if value in valueCounts:
-			valueCounts[value] += 1
+	# If testDependentVar is missing, assume it is because it's the same as trainDependentVar
+	if testDependentVar is None and isinstance(trainDependentVar, (str, int, long)):
+		testDependentVar = trainDependentVar
+	elif testDependentVar is None:
+		raise ArgumentException("Missing testDependentVar in runAndTestOneVsAll")
+
+	#Remove true labels from from training set, if not already separated
+	if isinstance(trainDependentVar, (str, int, long)):
+		trainDependentVar = trainX.extractFeatures(trainDependentVar)
+
+	#Remove true labels from test set, if not already separated
+	if isinstance(testDependentVar, (str, int, long)):
+		testDependentVar = testX.extractFeatures(testDependentVar)
+	else:
+		testTrueLabels = testDependentVar
+
+	# Get set of unique class labels
+	labelVector = trainX.copyFeatures([trainDependentVar])
+	labelVector.transpose()
+	labelSet = list(set(labelVector.toListOfLists()[0]))
+
+	#if we are logging this run, we need to start the timer
+	if sendToLog:
+		timer = Stopwatch()
+		timer.start('train')
+
+	# For each class label in the set of labels:  convert the true
+	# labels in trainDependentVar into boolean labels (1 if the point
+	# has 'label', 0 otherwise.)  Train a classifier with the processed
+	# labels and get predictions on the test set.
+	rawPredictions = None
+	for label in labelSet:
+		def relabeler(point):
+			if point[0] != label:
+				return 0
+			else: return 1
+
+		trainLabels = trainDependentVar.applyFunctionToEachPoint(relabeler)
+		oneLabelResults = run(algorithm, trainX, testX, output=None, dependentVar=trainLabels, arguments=arguments, sendToLog=False)
+		#put all results into one BaseData container, of the same type as trainX
+		if rawPredictions is None:
+			rawPredictions = oneLabelResults
+			#as it's added to results object, rename each column with its corresponding class label
+			rawPredictions.renameFeatureName(0, str(label))
 		else:
-			valueCounts[value] = 1
+			#as it's added to results object, rename each column with its corresponding class label
+			oneLabelResults.renameFeatureName(0, str(label))
+			rawPredictions.appendFeatures(oneLabelResults)
 
-	return valueCounts
 
-def extractWinningPrediction(predictions):
+	# We now have a data object with a confidence score for each possible label.  We want to reduce
+	# that to a prediction: predict whichever label has the highest score.  First we get the index 
+	# of the winning label for each row.  Then we translate that index into the label from the original
+	# data set.
+	predictionLabelIndices = rawPredictions.applyFunctionToEachPoint(extractWinningPredictionIndex)
+	predictionLabels = predictionLabelIndices.applyFunctionToEachPoint(lambda point: rawPredictions.featureNamesInverse[int(point[0])])
+
+
+	#if we are logging this run, we need to stop the timer
+	if sendToLog:
+		timer.stop('train')
+
+	#now we need to compute performance metric(s) for the set of winning predictions
+	results = computeMetrics(testTrueLabels, None, predictionLabels, performanceMetricFuncs)
+
+	# Send this run to the log, if desired
+	if sendToLog:
+		logManager = LogManager()
+		logManager.logRun(trainX, testX, algorithm, results, timer, extraInfo=arguments)
+
+	return results
+
+def extractWinningPredictionLabel(predictions):
 	"""
-	TODO: write docstring
+	Provided a list of tournament winners (class labels) for one point/row in a test set,
+	choose the label that wins the most tournaments.  Returns the winning label.
 	"""
 	#Count how many times each class won
 	#predictionCounts = valueCounter(predictions)
@@ -186,6 +277,27 @@ def extractWinningPrediction(predictions):
 	#TODO: what if there are ties?
 	return max(predictionCounts.iterkeys(), key=(lambda key: predictionCounts[key]))
 
+
+def extractWinningPredictionIndex(predictionScores):
+	"""
+	Provided a list of confidence scores for one point/row in a test set,
+	return the index of the column (i.e. label) of the highest score.  If
+	no score in the list of predictionScores is a number greater than negative
+	infinity, returns None.  
+	"""
+
+	maxScore = float("-inf")
+	maxScoreIndex = -1
+	for i in range(len(predictionScores)):
+		score = predictionScores[i]
+		if score > maxScore:
+			maxScore = score
+			maxScoreIndex = i
+
+	if maxScoreIndex == -1:
+		return None
+	else:
+		return maxScoreIndex
 
 
 #TODO this is a helper, move to utilities package?
