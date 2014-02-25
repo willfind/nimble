ae38f9a2b0893a5fb6cb4d69c298cedf2a494000
dan-jw <dan@gimbeltech.com>
2014-02-24 21:53:39 -0500

    Made allowImports() os independent.

--- examples/allowImports.py
+++ examples/allowImports.py
@@ -8,8 +8,9 @@ called from this folder aware of the package structure around it
 # PEP 366 'boilerplate', plus the necessary import of the top level package
 def boilerplate():
 	import sys
+	import os
 	# add UML parent directory to sys.path
-	sys.path.append(sys.path[0].rsplit('/',2)[0])
+	sys.path.append(sys.path[0].rsplit(os.path.sep,2)[0])
 	import UML
 	import UML.examples
 	__package__ = "UML.examples"
