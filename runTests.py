8224fa984238f9fe71f09ce97b0ddacd38e7bea1
Trevor Burns <trevorpburns@gmail.com>
2014-08-04 20:23:11 -0400

    Modified runTests so that by default it captures stderr as well as stdout

--- runTests.py
+++ runTests.py
@@ -6,11 +6,19 @@ UML. Run as main to execute.
 
 """
 
+import warnings
 import inspect
 import os
 import nose
 import sys
 from nose.plugins.base import Plugin
+
+import nose.pyversion
+#import pdb
+#pdb.set_trace()
+#print dir(nose.pyversion)
+from nose.util import ln
+from StringIO import StringIO
 
 class ExtensionPlugin(Plugin):
 
@@ -35,6 +43,92 @@ class ExtensionPlugin(Plugin):
         return True
 
 
+class CaptureError(Plugin):
+    """
+    Error output capture plugin. This plugin captures stderr during test
+    execution, appending any output captured to the error or failure output,
+    should the test fail or raise an error.
+
+    Modified from nose's builtin 'Capture' plugin for capturing stdout
+    """
+    enabled = True
+    env_opt = 'NOSE_NOCAPTUREERROR'
+    name = 'CaptureError'
+    score = 1600
+
+    def __init__(self):
+        self.stderr = []
+        self._buf = None
+
+    def options(self, parser, env):
+        """Register commandline options"""
+        parser.add_option(
+            "--nocaptureerror", action="store_false",
+            default=not env.get(self.env_opt), dest="captureerror",
+            help="Don't capture stderr (any stderr output "
+            "will be printed immediately) [NOSE_NOCAPTURE]")
+
+    def configure(self, options, conf):
+        """Configure plugin. Plugin is enabled by default."""
+        self.conf = conf
+        if not options.captureerror:
+            self.enabled = False
+
+    def afterTest(self, test):
+        """Clear capture buffer. """
+        self.end()
+        self._buf = None
+
+    def begin(self):
+        """Replace sys.stderr with capture buffer."""
+        self.start() # get an early handle on sys.stderr
+
+    def beforeTest(self, test):
+        """Flush capture buffer."""
+        self.start()
+
+    def formatError(self, test, err):
+        """Add captured output to error report."""
+        test.capturedOutput = output = self.buffer
+        self._buf = None
+        if not output:
+            # Don't return None as that will prevent other
+            # formatters from formatting and remove earlier formatters
+            # formats, instead return the err we got
+            return err
+        ec, ev, tb = err
+        return (ec, self.addCaptureToErr(ev, output), tb)
+
+    def formatFailure(self, test, err):
+        """Add captured output to failure report."""
+        return self.formatError(test, err)
+
+    def addCaptureToErr(self, ev, output):
+        return u'\n'.join([str(ev), ln(u'>> begin captured stderr <<'),
+                           str(output), ln(u'>> end captured stderr <<')])
+
+    def start(self):
+        self.stderr.append(sys.stderr)
+        self._buf = StringIO()
+        sys.stderr = self._buf
+
+    def end(self):
+        if self.stderr:
+            sys.stderr = self.stderr.pop()
+
+    def finalize(self, result):
+        """Restore stderr."""
+        while self.stderr:
+            self.end()
+
+    def _get_buffer(self):
+        if self._buf is not None:
+            return self._buf.getvalue()
+
+    buffer = property(_get_buffer, None, None,
+                      """Captured stderr output.""")        
+
+
 if __name__ == '__main__':
     # any args passed to this script will be passed down into nose
     args = sys.argv
@@ -46,6 +140,10 @@ if __name__ == '__main__':
     workingDirDef = ["-w", UMLPath]
     args.extend(workingDirDef)
 
-    nose.run(addplugins=[ExtensionPlugin()], argv=args)
+    # suppress all warnings -- nosetests only captures std out, not stderr,
+    # and there are some tests that call learners in unfortunate ways, causing
+    # ALOT of annoying warnings.
+    with warnings.catch_warnings():
+        nose.run(addplugins=[ExtensionPlugin(), CaptureError()], argv=args)
     exit(0)
 
