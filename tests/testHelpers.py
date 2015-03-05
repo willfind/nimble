e43e7a4c163cf19786993857ad14c44f9365460e
Trevor Burns <trevorpburns@gmail.com>
2015-03-05 15:08:20 -0500

    Moved two unit tests for helper functions in helpers.py to the correct test file

--- tests/testHelpers.py
+++ tests/testHelpers.py
@@ -542,3 +542,48 @@ def test_cocmputeMetrics_multiple_metrics_disallowed():
 
 	metricFunctions = [UML.calculate.cosineSimilarity, rootMeanSquareError]
 	computeMetrics(origObj, None, outObj, metricFunctions)
+
+
+
+def testExtractWinningPredictionLabel():
+	"""
+	Unit test for extractWinningPrediction function in runner.py
+	"""
+	predictionData = [[1, 3, 3, 2, 3, 2], [2, 3, 3, 2, 2, 2], [1, 1, 1, 1, 1, 1], [4, 4, 4, 3, 3, 3]]
+	BaseObj = createData('Matrix', predictionData)
+	BaseObj.transpose()
+	predictions = BaseObj.applyToFeatures(extractWinningPredictionLabel, inPlace=False)
+	listPredictions = predictions.copyAs(format="python list")
+	
+	assert listPredictions[0][0] - 3 == 0.0
+	assert listPredictions[0][1] - 2 == 0.0
+	assert listPredictions[0][2] - 1 == 0.0
+	assert (listPredictions[0][3] - 4 == 0.0) or (listPredictions[0][3] - 3 == 0.0)
+
+
+
+def testGenerateAllPairs():
+	"""
+	Unit test function for testGenerateAllPairs
+	"""
+	testList1 = [1, 2, 3, 4]
+	testPairs = generateAllPairs(testList1)
+	print testPairs
+
+	assert len(testPairs) == 6
+	assert ((1, 2) in testPairs) or ((2, 1) in testPairs)
+	assert not (((1, 2) in testPairs) and ((2, 1) in testPairs))
+	assert ((1, 3) in testPairs) or ((3, 1) in testPairs)
+	assert not (((1, 3) in testPairs) and ((3, 1) in testPairs))
+	assert ((1, 4) in testPairs) or ((4, 1) in testPairs)
+	assert not (((1, 4) in testPairs) and ((4, 1) in testPairs))
+	assert ((2, 3) in testPairs) or ((3, 2) in testPairs)
+	assert not (((2, 3) in testPairs) and ((3, 2) in testPairs))
+	assert ((2, 4) in testPairs) or ((4, 2) in testPairs)
+	assert not (((2, 4) in testPairs) and ((4, 2) in testPairs))
+	assert ((3, 4) in testPairs) or ((4, 3) in testPairs)
+	assert not (((3, 4) in testPairs) and ((4, 3) in testPairs))
+
+	testList2 = []
+	testPairs2 = generateAllPairs(testList2)
+	assert testPairs2 is None
