d8a3d16d6fec0ef0f67fc65bf78e81ad21d229ce
Trevor Burns <trevorpburns@gmail.com>
2015-08-05 18:23:46 -0400

    Cleanup in cross validation unit testing.
    Includes corrections to names and comments, removal of repetition,
    and easy deplication of tests for wider coverage.

--- tests/testCrossValidate.py
+++ tests/testCrossValidate.py
@@ -43,40 +43,42 @@ def _randomLabeledDataSet(dataType='Matrix', numPoints=50, numFeatures=5, numLab
 
 def test_crossValidate_XY_unchanged():
 	"""assert that after running cross validate on datasets passed to 
-	X and Y, the original data is unchanged"""
+	X and Y, the original data is unchanged
+
+	"""
 	classifierAlgo = 'Custom.KNNClassifier'
 	X, Y = _randomLabeledDataSet(numLabels=5)
 	copyX = X.copy()
 	copyY = Y.copy()
 	result = crossValidate(classifierAlgo, X, Y, fractionIncorrect, {}, numFolds=5)
-	assert X.hashCode() == copyX.hashCode()
-	assert Y.hashCode() == copyY.hashCode()
+	assert X == copyX
+	assert Y == copyY
 
 
+def test_crossValidate_callable():
+	"""tests that crossValidate is callable on different learners with
+	different UML data types
 
-def test_crossValidate_runs():
-	"""tests that crossValidate gives results (in form of float) 
-	for different algorithms
-	and different data types (children of base)
 	"""
 	#just scrap data to make sure it doesn't crash
-	numLabelsInSet = 3
-	numPointsInSet = 50
-	#todo add other data types - currently crashes in sklearn interface for list and sparse
-	for dType in ['Matrix',]:
-		X, Y = _randomLabeledDataSet(numPoints=numPointsInSet, numLabels=numLabelsInSet, dataType=dType)	
+	numLabels = 3
+	numPoints = 10
+	# todo add other data types - currently crashes in sklearn interface for
+	# list and sparse
+	for dType in ['Sparse']:
+		X, Y = _randomLabeledDataSet(numPoints=numPoints, numLabels=numLabels, dataType=dType)
+
 		classifierAlgos = ['Custom.KNNClassifier']
 		for curAlgo in classifierAlgos:
 			result = crossValidate(curAlgo, X, Y, fractionIncorrect, {}, numFolds=3)
 			assert isinstance(result, float)
 
-
 		#With regression dataset (no repeated labels)
-		X, Y = _randomLabeledDataSet(numLabels=None, dataType=dType)	
-		classifierAlgos = ['Custom.RidgeRegression']
-		for curAlgo in classifierAlgos:
-			result = crossValidate(curAlgo, X, Y, meanAbsoluteError, {}, numFolds=3)
-			assert isinstance(result, float)
+			X, Y = _randomLabeledDataSet(numPoints=numPoints, numLabels=None, dataType=dType)
+			classifierAlgos = ['Custom.RidgeRegression']
+			for curAlgo in classifierAlgos:
+				result = crossValidate(curAlgo, X, Y, meanAbsoluteError, {}, numFolds=3)
+				assert isinstance(result, float)
 		
 
 def _assertClassifierErrorOnRandomDataPlausible(actualError, numLabels, tolerance=.1):
@@ -140,16 +142,18 @@ def test_crossValidate_reasonable_results():
 	assert X.featureCount == 4
 	X.setFeatureNames(['X1', 'X2', 'X3', 'Y'])
 	result = crossValidate(regressionAlgo, X, 'Y', meanAbsoluteError, {}, numFolds=5)
+	assert X.featureCount == 4
 	#assert error essentially zero since there's no noise
 	assert result < .001
 
 
 def test_crossValidate_2d_api_check():
 	"""Check that crossValidate is callable with 2d data given to the Y argument
+	
 	"""
-	#assert that for an easy dataset (no noise, overdetermined linear hyperplane!), 
-	#crossValidated error is perfect 
-	#for all folds, with simple LinearRegression
+	# using an easy dataset (no noise, overdetermined linear hyperplane!),
+	# check that crossValidated error is perfect for all folds, with simple
+	# LinearRegression
 	regressionAlgo = 'Custom.RidgeRegression'
 
 	#make random data set where all points lie on a linear hyperplane
@@ -164,17 +168,43 @@ def test_crossValidate_2d_api_check():
 	metric = meanFeaturewiseRootMeanSquareError
 	result = crossValidate(regressionAlgo, X, Y, metric, {}, numFolds=5)
 	#assert error essentially zero since there's no noise
+	assert isinstance(result, float)
 	assert result < .001
 
 	index = X.featureCount
-	X.appendFeatures(Y)
-	X.setFeatureNames(['X1', 'X2', 'X3', 'Y1', 'Y2'])
-	result = crossValidate(regressionAlgo, X, [index, 'Y2'], metric, {}, numFolds=5)
+	combined = X.copy()
+	combined.appendFeatures(Y)
+	combined.setFeatureNames(['X1', 'X2', 'X3', 'Y1', 'Y2'])
+	result = crossValidate(regressionAlgo, combined, [index, 'Y2'], metric, {}, numFolds=5)
 	#assert error essentially zero since there's no noise
+	assert isinstance(result, float)
 	assert result < .001
 
+	# repeat for crossValidateReturnAll
+	result = crossValidateReturnAll(regressionAlgo, X, Y, metric, {}, numFolds=5)
+	assert len(result) == 1
+	assert len(result[0]) == 2
+	assert isinstance(result[0][0], dict) and result[0][0] == {}
+	assert isinstance(result[0][1], float) and result[0][1] < .001
+
+	result = crossValidateReturnAll(regressionAlgo, combined, [index, 'Y2'], metric, {}, numFolds=5)
+	assert len(result) == 1
+	assert len(result[0]) == 2
+	assert isinstance(result[0][0], dict) and result[0][0] == {}
+	assert isinstance(result[0][1], float) and result[0][1] < .001
+
+	# repeat for crossValidateReturnBest
+	result = crossValidateReturnBest(regressionAlgo, X, Y, metric, {}, numFolds=5)
+	assert len(result) == 2
+	assert isinstance(result[0], dict) and result[0] == {}
+	assert isinstance(result[1], float) and result[1] < .001
+
+
+
 def test_crossValidate_2d_Non_label_scoremodes_disallowed():
-	"""Check that crossValidate is callable with 2d data given to the Y argument
+	"""
+	Cross validation methods on 2d label data disallow non-default scoreModes
+
 	"""
 	#assert that for an easy dataset (no noise, overdetermined linear hyperplane!), 
 	#crossValidated error is perfect 
@@ -230,11 +260,12 @@ def test_crossValidate_2d_Non_label_scoremodes_disallowed():
 
 @attr('slow')
 @nose.with_setup(UML.randomness.startAlternateControl, UML.randomness.endAlternateControl)
-def test_crossValidateShuffleSeed():
+def test_crossValidate_foldingRandomness():
 	"""Assert that for a dataset, the same algorithm will generate the same model 
 	(and have the same accuracy) when presented with identical random state (and
 	therefore identical folds).
 	Assert that the model is different when the random state is different
+
 	"""
 	numTrials = 5
 	for _ in xrange(numTrials):
@@ -254,51 +285,39 @@ def test_crossValidateShuffleSeed():
 	assert resultOne != resultThree
 
 
-def _randomLabeledDataSet(dataType='Matrix', numPoints=100, numFeatures=5, numLabels=3):
-	"""returns a tuple of two data objects of type dataType
-	the first object in the tuple contains the feature information ('X' in UML language)
-	the second object in the tuple contains the labels for each feature ('Y' in UML language)
-	"""
-	if numLabels is None:
-		labelsRaw = [[pythonRandom.random()] for _x in xrange(numPoints)]
-	else:  # labels data set
-		labelsRaw = [[int(pythonRandom.random()*numLabels)] for _x in xrange(numPoints)]
-
-	rawFeatures = [[pythonRandom.random() for _x in xrange(numFeatures)] for _y in xrange(numPoints)]
-
-	return (createData(dataType, rawFeatures), createData(dataType, labelsRaw))
-
-
 @attr('slow')
 @nose.with_setup(UML.randomness.startAlternateControl, UML.randomness.endAlternateControl)
 def test_crossValidateReturnAll():
-	"""assert that KNeighborsClassifier generates results with default arguments
+	"""Check basic properties of crossValidateReturnAll
+
+	assert that default arguments will be filled in by the function
 	assert that having the same function arguments yields the same results.
 	assert that return all gives a cross validated performance for all of its 
 	parameter permutations
 	"""
 	X, Y = _randomLabeledDataSet(numPoints=50, numFeatures=10, numLabels=5)
-	#try with no extra arguments at all:
+	#try with no extra arguments at all; yet we know an argument exists (k):
 	result = crossValidateReturnAll('Custom.KNNClassifier', X, Y, fractionIncorrect)
 	assert result
 	assert 1 == len(result)
 	assert result[0][0] == {}
-	#try with some extra elements but all after default
+	#try with some extra elements, including the default
 	result = crossValidateReturnAll('Custom.KNNClassifier', X, Y, fractionIncorrect, k=(1,2,3))
 	assert result
 	assert 3 == len(result)
 
-	#since the same seed is used, and these calls are effectively building the same arguments, (p=2 is default for algo)
-	#the scores in results list should be the same (though the keys will be different (one the second will have 'p':2 in the keys as well))
+	# since the same seed is used, and these calls are effectively building the
+	# same arguments, the scores in results list should be the same, though
+	# ordered differently
 	seed = UML.randomness.pythonRandom.randint(0, sys.maxint)
 	UML.setRandomSeed(seed)
-	resultDifferentNeighbors = crossValidateReturnAll('Custom.KNNClassifier', X, Y, fractionIncorrect, k=(1,2,3,4,5))
+	result1 = crossValidateReturnAll('Custom.KNNClassifier', X, Y, fractionIncorrect, k=(1,2,3,4,5))
 	UML.setRandomSeed(seed)
-	resultDifferentNeighborsButSameCombinations = crossValidateReturnAll('Custom.KNNClassifier', X, Y, fractionIncorrect, k=(1,2,3,4,5))
+	result2 = crossValidateReturnAll('Custom.KNNClassifier', X, Y, fractionIncorrect, k=(1,5,4,3,2))
 	#assert the the resulting SCORES are identical
 	#uncertain about the order
-	resultOneScores = [curEntry[1] for curEntry in resultDifferentNeighbors]
-	resultTwoScores = [curEntry[1] for curEntry in resultDifferentNeighborsButSameCombinations]
+	resultOneScores = [curEntry[1] for curEntry in result1]
+	resultTwoScores = [curEntry[1] for curEntry in result2]
 	resultsOneSet = set(resultOneScores)
 	resultsTwoSet = set(resultTwoScores)
 	assert resultsOneSet == resultsTwoSet
@@ -306,8 +325,8 @@ def test_crossValidateReturnAll():
 	#assert results have the expected data structure:
 	#a list of tuples where the first entry is the argument dict
 	#and second entry is the score (float)
-	assert isinstance(resultDifferentNeighbors, list)
-	for curResult in resultDifferentNeighbors:
+	assert isinstance(result1, list)
+	for curResult in result1:
 		assert isinstance(curResult, tuple)
 		assert isinstance(curResult[0], dict)
 		assert isinstance(curResult[1], float)
