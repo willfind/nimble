abd0c9bcf4e1ce0b4e7c73c1ee8722754e996f5e
DevinShanahan <dshanahan88@gmail.com>
2018-08-19 09:06:16 +0200

    return list of new feature names

--- data/base.py
+++ data/base.py
@@ -527,6 +527,8 @@ class Base(object):
         toConvert.extractFeatures([varName])
         self.appendFeatures(toConvert)
 
+        return toConvert.getFeatureNames()
+
 
     def transformFeatureToIntegers(self, featureToConvert):
         """
