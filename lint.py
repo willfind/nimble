ba40d395b854ab42e79af20582cd609366254209
DevinShanahan <dshanahan88@gmail.com>
2019-02-07 13:28:39 -0700

    add more not-required; add docstrings; py2 fix

--- lint.py
+++ lint.py
@@ -18,69 +18,90 @@ from pylint.lint import Run
 from pylint import epylint as lint
 
 # pylint symbols that can be ignored for minimum linter requirements
-REQ_IGNORE = [
-    'invalid-name',
-    'too-many-lines',
-    'too-many-locals',
-    'too-many-branches',
-    'too-many-statements',
-    'too-many-return-statements',
-    'too-many-instance-attributes',
-    'comparison-with-itself',
-    'protected-access',
-    'pointless-statement',
-    'broad-except',
-    'no-else-return',
-    'len-as-condition',
-    'consider-using-enumerate',
-    'consider-using-in',
-    'protected-member',
-    'unused-argument',
-    'duplicate-code',
-    'no-self-use',
-    'chained-comparison',
-]
+def notRequired(warning):
+    """
+    Classify a warning as not required.
+
+    Warnings regarding 'too-many-*' or 'too-few-*', warnings that state
+    'consider-*' and any warnings in the reqIgnore list will return
+    True, meaning they are NOT required.
+    """
+    reqIgnore = [
+        'broad-except',
+        'chained-comparison',
+        'comparison-with-itself',
+        'duplicate-code',
+        'fixme',
+        'invalid-name',
+        'len-as-condition',
+        'no-else-return',
+        'no-self-use',
+        'pointless-statement',
+        'protected-access',
+        'protected-member',
+        'unnecessary-pass',
+        'unused-argument',
+    ]
+    return (warning.startswith('too')
+            or warning.startswith('consider')
+            or warning in reqIgnore)
+
 
 def getOutputs(commandString):
+    """
+    Run pylint and catch the output.
+
+    Parameters
+    ----------
+        commandString : str
+            The arguments from the command line as a string.
+    """
     pylint_stdout, pylint_stderr = lint.py_run(commandString, return_std=True)
     stderr = pylint_stderr.readlines()
-    if stderr:
+    # python2 is writing the config file location to stderr, so ignore that
+    if stderr and not stderr[0].startswith('Using config file'):
+        print("FAILURE: The following error occurred:")
         print("".join(stderr), file=sys.stderr)
         sys.exit()
     return pylint_stdout
 
-def jsonToDict(file):
+def jsonToDict(jsonFile):
+    """
+    Parse the json file from pylint and convert to a dict.
+    """
     dictionary = {}
 
-    lines = file.readlines()
+    lines = jsonFile.readlines()
     if lines:
         try:
             dictionary = json.loads("".join(lines))
         except Exception:
             # if can't load json format, stdout contains reason this failed
+            print("FAILURE: The following error occurred:")
             print("".join(lines), file=sys.stderr)
             sys.exit()
 
     return dictionary
 
 def analyzeWarnings(commandString):
+    """
+    Run pylint and convert the output to a dictionary.
+    """
     out = getOutputs(commandString)
     outDict = jsonToDict(out)
 
     return outDict
 
 def printWarningSummary(warnings):
+    """
+    Parse and organize warnings then print a summary.
+    """
     consider = []
     required = []
-    reqCount = 0
-    errCount = 0
     for w in warnings:
-        if w['type'] in ['error', 'fatal']:
-            errCount += 1
-        if w['symbol'] in REQ_IGNORE:
+        if notRequired(w['symbol']):
             consider.append(w)
         else:
-            reqCount += 1
             required.append(w)
 
     warning = "{module}:{line}: {message} ({symbol})"
@@ -89,18 +110,24 @@ def printWarningSummary(warnings):
         print('----------------')
         for warn in consider:
             print(warning.format(module=warn['module'], line=warn['line'],
-                                 symbol=warn['symbol'], message=warn['message']))
+                                 symbol=warn['symbol'],
+                                 message=warn['message']))
         print("\n")
+    reqOnly = []
     errors = []
     if required:
-        print('REQUIRED CHANGES')
-        print('----------------')
         for req in required:
             if req['type'] in ['error', 'fatal']:
                 errors.append(req)
             else:
-                print(warning.format(module=req['module'], line=req['line'],
-                                     symbol=req['symbol'], message=req['message']))
+                reqOnly.append(req)
+
+    if reqOnly:
+        print('REQUIRED CHANGES')
+        print('----------------')
+        for req in reqOnly:
+            print(warning.format(module=req['module'], line=req['line'],
+                                 symbol=req['symbol'], message=req['message']))
         print("\n")
 
     if errors:
@@ -108,24 +135,24 @@ def printWarningSummary(warnings):
         print('------')
         for err in errors:
             print(warning.format(module=err['module'], line=err['line'],
-                                     symbol=err['symbol'], message=err['message']))
+                                 symbol=err['symbol'], message=err['message']))
         print("\n")
 
     print('SUMMARY')
     print('-------')
-    print('{} total warnings and errors'.format(len(consider) + reqCount))
-    if len(consider) > 0:
+    total = len(consider) + len(reqOnly) + len(errors)
+    print('{} total warnings and errors'.format(total))
+    if consider:
         msg = '{} of the warnings should be reviewed; '.format(len(consider))
         msg += 'they may or may not require correction.'
         print(msg)
         print('    See CONSIDER CHANGES section above.')
-    if reqCount > 0:
-        if reqCount - errCount > 0:
-            print('{} of the warnings require correction.'.format(reqCount - errCount))
-            print('    See REQUIRED CHANGES section above.')
-        if errCount > 0:
-            print('{} are errors which must be corrected.'.format(errCount))
-            print('    See ERRORS section above.')
+    if reqOnly:
+        print('{} of the warnings require correction.'.format(len(reqOnly)))
+        print('    See REQUIRED CHANGES section above.')
+    if errors:
+        print('{} are errors which must be corrected.'.format(len(errors)))
+        print('    See ERRORS section above.')
     else:
         print('*This code satisfies the UML minimum linter requirements*')
 
@@ -141,7 +168,7 @@ if __name__ == '__main__':
         if arg.startswith("--output-format"):
             # Use pylint output if --output-format specified
             Run(configOptions)
-        elif arg in ['--version','-h', '--help', '--long-help']:
+        elif arg in ['--version', '-h', '--help', '--long-help']:
             # use of Run prints these correctly
             Run([arg])
         elif arg.startswith('--rcfile'):
@@ -157,5 +184,5 @@ if __name__ == '__main__':
 
     command = " ".join(configOptions)
 
-    warnings = analyzeWarnings(command)
-    printWarningSummary(warnings)
+    getWarnings = analyzeWarnings(command)
+    printWarningSummary(getWarnings)
