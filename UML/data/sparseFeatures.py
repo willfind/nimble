e518f0371efdff7e38f2817d33a056e9a33a12da
DevinShanahan <dshanahan88@gmail.com>
2019-05-23 07:29:41 -0600

    Sparse _add_implementation in batches

--- UML/data/sparseFeatures.py
+++ UML/data/sparseFeatures.py
@@ -31,47 +31,6 @@ class SparseFeatures(SparseAxis, Features):
     # Structural implementations #
     ##############################
 
-    def _add_implementation(self, toAdd, insertBefore):
-        """
-        Insert the features from the toAdd object to the right of the
-        provided index in this object, the remaining points from this
-        object will continue to the right of the inserted points.
-        """
-        self._source._sortInternal('feature')
-        newData = []
-        newRow = []
-        newCol = []
-        # add original data until insert location
-        for i, col in enumerate(self._source.data.col):
-            if col < insertBefore:
-                newRow.append(self._source.data.row[i])
-                newCol.append(col)
-                newData.append(self._source.data.data[i])
-            else:
-                break
-        # add inserted data with adjusted col
-        splitLength = len(newCol)
-        for i, col in enumerate(toAdd.data.col):
-            newRow.append(toAdd.data.row[i])
-            newCol.append(col + insertBefore)
-            newData.append(toAdd.data.data[i])
-        # add remaining original data with adjusted col
-        for i, col in enumerate(self._source.data.col[splitLength:]):
-            newRow.append(self._source.data.row[splitLength:][i])
-            newCol.append(col + len(toAdd.features))
-            newData.append(self._source.data.data[splitLength:][i])
-        # handle conflicts between original dtype and inserted data
-        try:
-            newData = numpy.array(newData, dtype=self._source.data.dtype)
-        except ValueError:
-            newData = numpy.array(newData, dtype=numpy.object_)
-
-        numNewCols = len(self._source.features) + len(toAdd.features)
-        shape = (len(self._source.points), numNewCols)
-        self._source.data = coo_matrix((newData, (newRow, newCol)),
-                                       shape=shape)
-        self._source._sorted = None
-
     # def _flattenToOne_implementation(self):
     #     self._source._sortInternal('feature')
     #     fLen = len(self._source.points)
