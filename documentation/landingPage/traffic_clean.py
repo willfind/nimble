e07764ff85acd8f296395bae3a9c904f28b4ceb3
DevinShanahan <dshanahan88@gmail.com>
2020-03-09 09:29:02 -0600

    traffic clean updates

--- documentation/landingPage/traffic_clean.py
+++ documentation/landingPage/traffic_clean.py
@@ -3,10 +3,13 @@ Cleaning Data
 
 Using nimble to prepare interstate traffic volume
 
-The dataset, `'Metro_Interstate_Traffic_Volume.csv'`, contains hourly data on
-interstate traffic volumes and features that may contribute to changes in
-traffic volume. Our goal here is to address several issues present in this
-raw dataset to make it suitable for supervised learning.
+The goal here is to address several issues present in the
+`'Metro_Interstate_Traffic_Volume.csv'` dataset to make it suitable for
+supervised learning. Each point in this dataset represents a point in
+time where 9 features are recorded. The features are variables which may
+affect the traffic volume on an interstate highway and the current
+traffic volume. A new point is recorded each time a change occurs in one
+of the weather forecast features.
 
 Reference:
 Dua, D. and Graff, C. (2019).
@@ -17,53 +20,70 @@ Link:
 https://archive.ics.uci.edu/ml/datasets/Metro+Interstate+Traffic+Volume
 """
 
+## Getting started
+
 from datetime import datetime
 
 import nimble
-from nimble import calculate
-
-# Each point in this dataset represents a point in time where 9 features
-# are recorded. The features are variables which may affect the traffic volume
-# on an interstate highway and a new point is recorded each time a change
-# occurs in one of the weather forecast features. Further details will be
-# provided on any relevant features as the dataset is cleaned.
-
-### Creating data
-# We start by creating a data object using `nimble.createData`.  Nimble has
-# four different data object types `Matrix`, `List`, `Sparse` and `DataFrame`
-# that all have the same functionality and can interact with each other.
-# `createData` can accept data in many forms, here we pass a string providing
-# the path to a csv file.  This csv file contains headers for each column, so
-# we set `featureNames` to True. After creating our object, we use some of it's
-# functionality to learn a little bit about our dataset.
 
 traffic = nimble.createData('Matrix', 'Metro_Interstate_Traffic_Volume.csv',
                             featureNames=True)
-print(traffic.features.getNames())
-print(traffic.summaryReport())
-# Choosing this point range because shows a holiday
-print(traffic[124:128, :])
+# kwargs for use with .show to generate output desired for markdown files
+showKwargs = {'includeObjectName': False, 'maxHeight': 9, 'maxWidth': 120}
+traffic.show("Raw traffic data", **showKwargs)
 
-### Cleaning data
-# First, we are going to split the `date_time` feature into four features;
-# `year`, `month`, `weekday`, and `hour`. This will give us numeric features
-# from the non-numeric `date_time` feature.  To accomplish this, we will write
-# a function to pass as the `rule` argument for `traffic.features.splitParsing`
-# that extracts the information we need from each `datetime` value.
+# Our data contains 48,204 points and 9 features, but some points and features
+# will need to be addressed before machine learning algorithms can be applied
+# to this data. The machine learning algorithms we plan to use require numeric
+# data and can be sensitive to outliers. Running `featureReport` can provide
+# a good starting point for cleaning the data.
 
-def dateTimeSplit(values):
-    dt = datetime.strptime(values, "%Y-%m-%d %H:%M:%S")
+print(traffic.featureReport())
+
+# Statistics could not be calculated for all features, indicating some are
+# non-numeric. The statistics for the numeric features also indicate that
+# some outlier values may be present.
+
+## Cleaning numeric data
+# 0 Kelvin in `temp` and 9831.3 mm in `rain_1h` indicate some outliers exist in
+# these features. Let's extract those values to decide how to proceed.
+
+def outlierIdentifier(pt):
+    if pt['temp'] == 0 or pt['rain_1h'] == 9831.3:
+        return True
+    return False
+
+outliers = traffic.points.extract(outlierIdentifier)
+
+print(traffic[:, ['temp', 'rain_1h']].featureReport())
+print('Number of points with outliers:', len(outliers.points))
+
+# After extracting those values, our `featureReport` statistics look much
+# more reasonable for those features. We can assume the 11 extracted points
+# contain recording errors so we will ignore `outliers` and continue with
+# the 48,193 points still remaining in `traffic`.
+
+## Cleaning non-numeric data
+
+# The values in the `date_time` feature are strings, but we can parse each
+# string to generate five new numeric features to replace this feature.
+def dateTimeSplit(value):
+    dt = datetime.strptime(value, "%Y-%m-%d %H:%M:%S")
     return [dt.year, dt.month, dt.day, dt.hour, dt.weekday()]
 
 traffic.features.splitByParsing('date_time', dateTimeSplit,
                                 ['year', 'month', 'day', 'hour', 'weekday'])
 
-# Our initial look at the data also showed the `holiday` feature contains
-# missing values. This feature contains a string of the holiday name if it
-# is a holiday, but only for the first point of that day. Our goal is to make
-# this a binary feature with 0 indicating a non-holiday and 1 indicating a
-# holiday for each hour of every day.
+traffic.show('New parsed features in traffic data', **showKwargs)
 
+# The `holiday` contains a holiday name string for the first point of each
+# holiday, all other values are missing.
+samplePts = slice(1369,1374)
+dateFts = ['holiday', 'year', 'month', 'day', 'hour']
+print(traffic[samplePts, dateFts])
+
+# We will transform the `holiday` feature to a numeric feature by writing a
+# function that assigns each point a new binary value in the `holiday` feature.
 # TODO using 1/0 for now until True/False can be loaded from csv
 currentHoliday = {'date': None}
 def holidayToBinary(point):
@@ -82,37 +102,26 @@ def holidayToBinary(point):
     return filledPt
 
 traffic.points.transform(holidayToBinary)
+print(traffic[samplePts, dateFts])
 
-# By default, nimble condenses the printed output of larger objects so we were
-# unable to see the `weather_main` and `weather_description` features. A quick
-# look indicates that they are very similar, but `weather_description` provides
-# more detail for our predictions. We will remove `weather_main` and transform
-# each of the 36 categories in `weather_description` to a binary feature so we
-# have numeric data for our learners.
+# The `weather_main` and `weather_description` features are very similar.
+weatherFts = ['weather_main', 'weather_description']
+print(traffic[samplePts, weatherFts])
 
-print(traffic[:4, ['weather_main', 'weather_description']])
+# While `weather_description` is more detailed, many of its unique values
+# represent a very small proportion of the data. So, we will utilize the more
+# general `weather_main` and remove `weather_description`. To make the string
+# values in `weather_main` suitable for machine learning, we will represent
+# each of the 11 unique values as 11 new binary features.
+traffic.features.delete('weather_description')
+newCols = traffic.replaceFeatureWithBinaryFeatures('weather_main')
 
-traffic.features.delete('weather_main')
-newCols = traffic.replaceFeatureWithBinaryFeatures('weather_description')
+print(traffic[samplePts, newCols[:3]])
 
-# Some values appear to be anomalies in the data, for example a small portion
-# of values in the `temp` feature are recorded as 0 Kelvin. Since only a
-# small number of points with anomalies exist, we will remove them. Our
-# `anomalyRemover` function returns `True` for any value beyond 8 standard
-# deviations from the mean indicating to `traffic.points.delete` to remove
-# the point containing the anomaly from the data.
+# Our dataset is now ready for machine learning.
+traffic.show('Cleaned traffic data', **showKwargs)
 
-means = traffic.features.statistics('mean')
-stDevs = traffic.features.statistics('standarddeviation')
-
-def anomalyRemover(pt):
-    for ft in ['temp', 'rain_1h', 'snow_1h']:
-        if abs((pt[ft] - means[ft]) / stDevs[ft]) > 8:
-            return True
-    return False
-
-traffic.points.delete(anomalyRemover)
-
-### Writing to a file
-# Now that our data is clean, we can write to a new csv file.
+## Writing to a file
+# So that we can load the cleaned data for our supervised learning example, we
+# will write it to a new csv file.
 traffic.writeFile('Metro_Interstate_Traffic_Volume_Cleaned.csv')
