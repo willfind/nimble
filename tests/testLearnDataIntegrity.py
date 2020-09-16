4a2f4a451ce0cc168f850437c8176040d07d70fd
DevinShanahan <dshanahan88@gmail.com>
2020-09-16 11:15:02 -0600

    axis name preservation for TL apply and normalizeData

--- tests/testLearnDataIntegrity.py
+++ tests/testLearnDataIntegrity.py
@@ -313,10 +313,12 @@ def testArgumentIntegrityTrainAndTestOnTrainingData():
 @patch_mergeArguments('nimble.core.interfaces.universal_interface')
 def testArgumentIntegrityTLApply():
     arguments = {'k': 1}
-    train = nimble.data('Matrix', [[0, 0, 0], [0, 1, 1], [1, 0, 2], [1, 1, 3]])
-    test = nimble.data('Matrix', [[0, 1], [1, 0]])
+    train = nimble.data('Matrix', [[0, 0, 0], [0, 1, 1], [1, 0, 2], [1, 1, 3]],
+                        pointNames=['0', '1', '2', '3'])
+    test = nimble.data('Matrix', [[0, 1], [1, 0]], pointNames=['4', '5'])
     tl = nimble.train('nimble.KNNClassifier', train, 2, arguments=arguments)
     pred = tl.apply(test)
+    assert pred.points.getNames() == testX.points.getNames()
 
 @raises(CalledFunctionException)
 @patch_mergeArguments('nimble.core.interfaces.universal_interface')
