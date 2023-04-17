1317df89450fa3795593ef6b7d49eb7500accef5
Niniola Adegboyega <nini.adegboyega@gmail.com>
2023-04-17 09:15:01 +0100

    some more fixes

--- tests/testData.py
+++ tests/testData.py
@@ -4253,8 +4253,8 @@ def test_converttoDataFrames_numpyStructuredArrays():
     dataArray = nimble.data(structArray)
     assert type(dataArray) == nimble.core.data.dataframe.DataFrame
     assert np.issubdtype(dataArray._data.dtypes[0], np.object_)
-    assert np.issubdtype(dataArray._data.dtypes[1], np.int_)
-    assert np.issubdtype(dataArray._data.dtypes[2], np.float_)
+    assert np.issubdtype(dataArray._data.dtypes[1], np.integer)
+    assert np.issubdtype(dataArray._data.dtypes[2], np.floating)
     assert dataArray.features.getNames() == ['name', 'age', 'weight']
 
 def test_tuplesArePoints_numpyStructuredArrays():
