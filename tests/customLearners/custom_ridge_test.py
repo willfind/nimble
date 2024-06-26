c7bd934a9fa9e64fd9e6e57c922dabfcc2aa4b8c
tpburns <trevorpburns@gmail.com>
2024-06-26 16:08:27 -0400

    Update custom_ridge_test.py
    Remove breakpoint

--- tests/customLearners/custom_ridge_test.py
+++ tests/customLearners/custom_ridge_test.py
@@ -22,7 +22,6 @@ from nimble.learners import RidgeRegression
 
 
 def testRidgeRegressionShapes():
-    import pdb; pdb.set_trace()
     """ Test ridge regression by checking the shapes of the inputs and outputs """
 
     data = [[0, 0, 0], [4, 3, 1], [12, 15, -3], ]
