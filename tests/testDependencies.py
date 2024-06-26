334da2a0b1367a082618dd08a7598b72509705ba
Niniola Adegboyega <nini.adegboyega@gmail.com>
2024-06-26 17:55:30 +0100

    removed pdb mention

--- tests/testDependencies.py
+++ tests/testDependencies.py
@@ -27,7 +27,6 @@ import tomli
 import importlib
 
 def test_deps_matchingSections():
-    import pdb; pdb.set_trace()
     """
     Check that the hard-coded sections of the Dependency objects match the
     structure of the optional dependencies in the pyproject file.
